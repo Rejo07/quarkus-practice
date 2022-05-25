70e1727e99f4fb41b2dcb63609193f5e5106ea88
Rejo07 <jjacob.c@saip.gov.sa>
2022-05-25 18:09:43 +0530

    Update

--- src/main/java/org/acme/employee/DepartmentResources.java
+++ src/main/java/org/acme/employee/DepartmentResources.java
@@ -1,11 +1,14 @@
 package org.acme.employee;
 
+import java.util.Objects;
+
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,6 +44,43 @@ public class DepartmentResources {
     public Response getDepartmentById(@PathParam("id") Long id) {
         DepartmentEntity departmentEntity = DepartmentEntity.findById(id);
         return Response.ok(departmentEntity).build();
+    }
+
+    @Path("/{id}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response updateDepartment(@PathParam("id") Long id, Department department) {
+        DepartmentEntity departmentEntity = DepartmentEntity.findById(id);
+
+        if (Objects.nonNull(department.getCode())) {
+            departmentEntity.code = department.getCode();
+        }
+        if (Objects.nonNull(department.getName())) {
+            departmentEntity.name = department.getName();
+        }
+        if (Objects.nonNull(department.getShortName())) {
+            departmentEntity.shortName = department.getShortName();
+        }
+
+        if (Objects.nonNull(department.getNotes())) {
+            departmentEntity.notes = department.getNotes();
+        }
+
+        if (Objects.nonNull(department.getCreatedBy())) {
+            departmentEntity.createdBy = department.getCreatedBy();
+        }
+        if (Objects.nonNull(department.getUpdatedBy())) {
+            departmentEntity.updatedBy = department.getUpdatedBy();
+        }
+        if (Objects.nonNull(department.getCreatedAt())) {
+            departmentEntity.createdAt = department.getCreatedAt();
+        }
+        if (Objects.nonNull(department.getUpdatedAt())) {
+            departmentEntity.updatedAt = department.getUpdatedAt();
+        }
+        return Response.noContent().build();
     }
 
     @Path("/{id}")
